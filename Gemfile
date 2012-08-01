c36d91d5231ba5740ca5cb1b07051405b767e43e
Gary Cagle <gcagle@bitstream.net>
2012-07-31 22:13:13 -0500

    improved prod build

--- Gemfile
+++ Gemfile
@@ -8,9 +8,12 @@ group :development, :test do
  gem 'rspec-rails', '2.10.0'
 end
 
+# Gems used only for assets and not required
+# in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails',   '~> 3.2.4'
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'uglifier', '1.2.3'
 end
 
 gem 'jquery-rails', '2.0.0'
@@ -21,8 +24,6 @@ end
 
 group :production do
   gem 'pg', '0.12.2'
-  gem 'uglifier'
-  gem 'therubyracer'
 end
 
 # To use ActiveModel has_secure_password # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,10 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+    Bundler.require(*Rails.groups(:assets => %w(development test )))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-#config.assets.initialize_on_precompile = false  # advice from heroku for rake precompile asset error
 
 module Sample2
   class Application < Rails::Application
@@ -62,6 +61,8 @@ module Sample2
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    #config.assets.initialize_on_precompile = false  # advice from heroku for rake precompile asset error
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
